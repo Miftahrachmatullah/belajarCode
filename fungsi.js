45cf7a7603b1a8129238f434909d900c1850a8b3
Miftahrachmatullah <mochamadmiftah34@gmail.com>
2026-04-04 14:56:08 +0700

    merevisi perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,11 +1,10 @@
-const add = (a, b) => a + b;
-console.log(add(10, 5));
+console.log("Hello World");
 
 const substract = (a, b) => a - b;
 console.log(substract(10, 5));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(10, 5));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(10, 5));
 
 const divide = (a, b) => a / b;
 console.log(divide(10, 5));
